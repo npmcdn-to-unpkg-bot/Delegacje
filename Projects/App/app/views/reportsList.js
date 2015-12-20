076bfd37ad96c37653e945a2414a4837464abdb8
kbarkowski <karol.barkowski@gmail.com>
2015-12-20 01:08:39 +0100

    dummy reports list loaded

--- Projects/App/app/views/reportsList.js
+++ Projects/App/app/views/reportsList.js
@@ -1,11 +1,17 @@
-﻿define(['knockout'], function (ko) {
+﻿define(['knockout', 'services/api'], function (ko, api) {
 
     var vm = {
         IsInitialized: ko.observable(false),
         Reports: ko.observableArray(),
 
         activate: function () {
-
+            api.get(api.url.BusinessTripsForUser())
+                .then(function (result) {
+                    vm.Reports(result);
+                })
+                .always(function () {
+                    vm.IsInitialized(true);
+                });
         }
     };
 
