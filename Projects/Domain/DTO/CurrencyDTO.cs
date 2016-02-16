421d276a374cf37c0b1ccbc17d5741bf9e1bc5f1
Maciej Surwiło <suruwa@gmail.com>
2016-02-16 09:33:27 +0100

    Currency changes

---
+++ Projects/Domain/DTO/CurrencyDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyAppsStudio.Delegacje.Domain.DTO
+{
+	public class CurrencyDTO
+	{
+		public string Code { get; set; }
+		public double ExchangeRate { get; set; }
+	}
+}
