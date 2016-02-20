8e65d5fe9f04df3e47a89c9b211a85d7bcc157a9
kbarkowski <karol.barkowski@gmail.com>
2016-02-20 02:56:44 +0100

    ui amended, css updated

--- Projects/App/wwwroot/system/landing.controller.js
+++ Projects/App/wwwroot/system/landing.controller.js
@@ -8,33 +8,54 @@
     LandingController.$inject = ['userReportsService', 'ngDialog', 'authenticationService', '$state'];
 
     function LandingController(userReportsService, ngDialog, authenticationService, $state) {
+        var clickedReport = null;
+
         var vm = this;
         vm.reports = userReportsService.reports;
-        vm.remove = function (report) {
+
+        vm.popupVisible = false;
+        vm.showPopup = function (event, report) {
+            clickedReport = report;
+
+            var targetTag = angular.element(document.querySelector('#table-menu'));
+            targetTag.css({
+                position: "fixed",
+                display: "block",
+                left: (event.clientX - 290) + 'px',
+                top: (event.clientY + 10) + 'px'
+            })
+
+            vm.popupVisible = true;
+        };
+        vm.hidePopup = function () {
+            vm.popupVisible = false;
+        };
+
+        vm.remove = function () {
             var warningDialog = ngDialog.open({
                 template: 'wwwroot/report/popups/remove.warning.template.html'
             });
 
             warningDialog.closePromise.then(function (data) {
                 if (data.value === true) {
-                    report.isRemoving = true;
-                    userReportsService.remove(report.Id);
+                    userReportsService.remove(clickedReport.Id);
                 }
             });            
         }
-        vm.copy = function (Id) {
-            userReportsService.copy(Id).then(function (result) {
+        vm.copy = function () {
+            userReportsService.copy(clickedReport.Id).then(function (result) {
                 vm.reports().push(result.data);
             });
         }
 
-        vm.edit = function (Id) {
-            $state.go('report-edit', { reportId: Id });
+        vm.edit = function () {
+            $state.go('report-edit', { reportId: clickedReport.Id });
         }
 
         userReportsService.reload();
 
-        if (!authenticationService.isAuthenticated())
+        if (!authenticationService.isAuthenticated()) {
             $state.go('login');
+        }
     }
 })();
