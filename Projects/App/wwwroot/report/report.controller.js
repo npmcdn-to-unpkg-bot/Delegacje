09e2374fe4b5bf5439d0c3a290003ca21994bfd4
Maciej Surwiło <suruwa@gmail.com>
2016-03-04 18:54:19 +0100

    51753: limit currency used for subsistence calculations

--- Projects/App/wwwroot/report/report.controller.js
+++ Projects/App/wwwroot/report/report.controller.js
@@ -239,7 +239,7 @@
         vm.SubsistenceCurrencyData = null;
         vm.InitializeSubsistences = function () {
             vm.GettingSubsistenceExchageRate = true;
-            currenciesService.getExchangeRate(vm.NewSubsistence.Country.Currency.Code, stringToDate(vm.NewSubsistence.StartDate))
+            currenciesService.getExchangeRate(vm.NewSubsistence.Country.LimitCurrency.Code, stringToDate(vm.NewSubsistence.StartDate))
             .then(function (data) {
                 vm.SubsistenceCurrencyData = data;
 
